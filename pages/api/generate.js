64cd0799b628b1cced2e593c36efbdc2c01945ad
danial.asaria <dasaria@usc.edu>
2022-11-13 10:30:51 -0800

    added loading, chakra ui text area

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -12,7 +12,7 @@ export default async function (req, res) {
     model: "text-davinci-002",
     prompt: generatePrompt(req.body.prompt),
     temperature: 0.6,
-    max_tokens: 40,
+    max_tokens: 100,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
+import React, { useState } from "react";
+
 import Head from "next/head";
+import { Spinner } from "@chakra-ui/react";
+// import ResizeTextarea from "react-textarea-autosize";
+import { Textarea } from "@chakra-ui/react";
 import styles from "./index.module.css";
-import { useState } from "react";
 
 export default function Home() {
   const [promptInput, setPromptInput] = useState("");
-  const [result, setResult] = useState();
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function onSubmit(event) {
     event.preventDefault();
+    setLoading(true);
     const response = await fetch("/api/generate", {
       method: "POST",
       headers: {
@@ -17,13 +23,37 @@ export default function Home() {
     });
     const data = await response.json();
     setResult(data.result);
+    setLoading(false);
+    // setModelOutput(data.result);
     setPromptInput("");
   }
+
+  const handleChange = (e) => {
+    setResult(e.target.value);
+  };
+
+  // const AutoResizeTextarea = React.forwardRef((props, ref) => {
+  //   return (
+  //     <Textarea
+  //       id="resultTextArea"
+  //       value={result}
+  //       onChange={handleChange}
+  //       resize="vertical"
+  //       placeholder="Welcome :)"
+  //       size="md"
+  //       as={ResizeTextarea}
+  //     />
+  //   );
+  // });
+
+  // const setModelOutput = (result) => {
+  //   document.getElementById("resultTextArea").value = result;
+  // };
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>USC Essay Writer</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
@@ -40,8 +70,24 @@ export default function Home() {
           />
           <input type="submit" value="Generate Essay" />
         </form>
-        <div className={styles.result}>{result}</div>
       </main>
+      <div className={styles.resultTextArea}>
+        {loading ? (
+          <div className={styles.spinner}>
+            <Spinner />
+          </div>
+        ) : (
+          <Textarea
+            id="resultTextArea"
+            value={result}
+            onChange={handleChange}
+            resize="vertical"
+            placeholder="Welcome :)"
+            size="md"
+            minHeight="400px"
+          />
+        )}
+      </div>
     </div>
   );
 }
